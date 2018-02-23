92fa169103cca644022e2842d58d868f39180f53
Matt Hernandez <maherna@microsoft.com>
2018-02-23 10:12:29 -0800

    Clean up code format to match modern standard.Use const and get rid of unnecessary things..

--- app/routes.js
+++ app/routes.js
@@ -1,57 +1,35 @@
-var Todo = require('./models/todo');
+const Todo = require('./models/todo')
 
 function getTodos(res) {
-    Todo.find(function (err, todos) {
+  Todo.find((err, todos) => {
+    if (err) return res.send(err)
+    res.json(todos)
+  })
+}
 
-        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if (err) {
-            res.send(err);
-        }
+module.exports = (app) => {
+  app.get('/api/todos', getTodos)
 
-        res.json(todos); // return all todos in JSON format
-    });
-};
+  app.post('/api/todos', (req, res) => {
+    Todo.create({
+      text: req.body.text,
+      done: false
+    }, (err, todo) => {
+      if (err) return res.send(err)
+      getTodos(res)
+    })
+  })
 
-module.exports = function (app) {
+  app.delete('/api/todos/:todo_id', (req, res) => {
+    Todo.remove({
+      _id: req.params.todo_id
+    }, (err, todo) => {
+      if (err) return res.send(err)
+      getTodos(res)
+    })
+  })
 
-    // api ---------------------------------------------------------------------
-    // get all todos
-    app.get('/api/todos', function (req, res) {
-        // use mongoose to get all todos in the database
-        getTodos(res);
-    });
-
-    // create todo and send back all todos after creation
-    app.post('/api/todos', function (req, res) {
-
-        // create a todo, information comes from AJAX request from Angular
-        Todo.create({
-            text: req.body.text,
-            done: false
-        }, function (err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            getTodos(res);
-        });
-
-    });
-
-    // delete a todo
-    app.delete('/api/todos/:todo_id', function (req, res) {
-        Todo.remove({
-            _id: req.params.todo_id
-        }, function (err, todo) {
-            if (err)
-                res.send(err);
-
-            getTodos(res);
-        });
-    });
-
-    // application -------------------------------------------------------------
-    app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
-};
+  app.get('*', (req, res) => {
+    res.sendFile(require('path').join(__dirname, '/public/index.html'))
+  })
+}
--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-// set up ======================================================================
-var express = require('express');
-var app = express(); 						// create our app w/ express
-var mongoose = require('mongoose'); 				// mongoose for mongodb
-var port = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+const express = require('express')
+const mongoose = require('mongoose')
+const database = require('./config/database')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
-// configuration ===============================================================
-mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
+const app = express()
+const port = process.env.PORT || 8080
 
-app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+mongoose.connect(database.localUrl)
+mongoose.connection.on('error', console.log.bind(console, 'Mongo Error:'))
 
+app.use(express.static('./public'))
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ 'extended': 'true' }))
+app.use(bodyParser.json())
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+app.use(methodOverride('X-HTTP-Method-Override'))
 
-// routes ======================================================================
-require('./app/routes.js')(app);
+require('./app/routes.js')(app)
 
-// listen (start app with node server.js) ======================================
-app.listen(port);
-console.log("App listening on port " + port);
+app.listen(port)
+console.log(`App listening on port ${port}`)
