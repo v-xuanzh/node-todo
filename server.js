3b836d86d5c2ebac0fc446f0b8c408bb31fa9f3e
Matt Hernandez <maherna@microsoft.com>
2018-02-26 08:40:18 -0800

    "Fix" the encoding.
    This is actually an error that is part of my demo.

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connection.on('error', console.log.bind(console, 'Mongo Error:'))
 
 app.use(express.static('./public'))
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({ 'extended': 'true' }))
+app.use(bodyParser.urlencoded({ 'extended': 'false' }))
 app.use(bodyParser.json())
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 app.use(methodOverride('X-HTTP-Method-Override'))
